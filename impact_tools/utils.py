00eb34b3e488df4c9e370d5b1e50314fe4b134ec
mmatito <magda.smat@gmail.com>
2026-07-24 11:38:00 +0200

    Added rich_force_colors helper to utils

---
+++ impact_tools/utils.py
@@ -0,0 +1,16 @@
+"""Shared helpers for impact-tools."""
+
+import os
+
+
+def rich_force_colors() -> bool | None:
+    """
+    Check if any environment variables are set to force Rich to use coloured output
+    """
+    if (
+        os.getenv("GITHUB_ACTIONS")
+        or os.getenv("FORCE_COLOR")
+        or os.getenv("PY_COLORS")
+    ):
+        return True
+    return None
